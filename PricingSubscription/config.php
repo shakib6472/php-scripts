b1fd1339b70ff363e7038f82453a899fcc98c98b
shakib6472 <123575818+shakib6472@users.noreply.github.com>
2024-09-08 14:25:29 +0600

    Add files via upload

---
+++ PricingSubscription/config.php
@@ -0,0 +1,7 @@
+<?php
+$SETTINGS["mysql_user"]='root';
+$SETTINGS["mysql_pass"]='';
+$SETTINGS["hostname"]='localhost';
+$SETTINGS["mysql_database"]='php_scripts';
+$SETTINGS["data_table"]='registrations';
+$SETTINGS["paypal_address"]='email@domain.com';
